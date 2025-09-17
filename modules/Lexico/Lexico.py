4d2cd89548e2017bcccd24bad4d4f3e24a0d3cf3
RechargeCG <richardcoelhokta@gmail.com>
2025-09-17 09:35:05 -0300

    Ops

--- modules/Lexico/Lexico.py
+++ modules/Lexico/Lexico.py
@@ -66,8 +66,6 @@ class Lexico(ILexico):
     def setStringMode(self):
         self.mode = LexicoModes.STRING
 
-    def 
-
     def generateOutput(self):
         """
         Gera o output de acordo com os dados de entrada
