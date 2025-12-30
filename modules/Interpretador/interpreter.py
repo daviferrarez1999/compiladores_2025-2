b256fdc758d5c200639773085c8711431a270a6c
RechargeCG <richardcoelhokta@gmail.com>
2025-12-30 17:32:43 -0300

    Tipos nas variáveis

--- modules/Interpretador/interpreter.py
+++ modules/Interpretador/interpreter.py
@@ -2,12 +2,12 @@ from frame import Frame
 import sys
 import shlex
 
-CODE: list[list[str]] = None  # Lista de instruções
-LABELS: dict[str,int] = None  # Lista de marcadores
-PC = 0                        # Program Counter
-STACK: list[Frame] = []       # Pilha de frames (chamadas de funções)
-GLOBALS = {'ra': None}        # Variáveis globais
-PARAMETERS = []               # Fila de parâmetros usado pela instrução PARAM
+CODE: list[list[str]] = None                    # Lista de instruções
+LABELS: dict[str,int] = None                    # Lista de marcadores
+PC = 0                                          # Program Counter
+STACK: list[Frame] = []                         # Pilha de frames (chamadas de funções)
+GLOBALS = {'ra': {'value': None, 'type': None}} # Variáveis globais
+PARAMETERS = []                                 # Fila de parâmetros usado pela instrução PARAM
 
 def read_code(dir):
     with open(dir, 'r', encoding='utf-8-sig') as file:
@@ -59,12 +59,9 @@ def is_number(val):
 def to_value(id):
     global GLOBALS, STACK
     
-    # id é número?
     val = is_number(id)
-    if val is not None: 
-        return val
-        
-    # id é uma posição de array?
+    if val is not None: return val
+    
     if '$' in id:
         name, pos = id.split('$')
         pos_val = int(to_value(pos))
@@ -72,60 +69,83 @@ def to_value(id):
         target = GLOBALS.get(name)
         if target is None and current_frame():
             target = current_frame().get_var(name)
-        
-        return target[pos_val] if target is not None else None
 
-    # id é uma váriável global?
+        target=target['value']
+        
+        item = target[pos_val] if target is not None else None
+        return item['value'] if isinstance(item, dict) else item
+
+    # Busca em Globais ou Frames
+    res = None
     if id in GLOBALS: 
-        return GLOBALS[id]
+        res = GLOBALS[id]
+    elif current_frame():
+        res = current_frame().get_var(id)
+
+    # Se encontramos um dicionário, retornamos apenas o valor bruto
+    if isinstance(res, dict):
+        return res['value']
     
-    frame = current_frame()
-    if frame:
-        val = frame.get_var(id)
-        if val is not None:
-            # Se a variável guarda o nome de outra (alias/ponteiro), resolve recursivo
-            return to_value(val) if isinstance(val, str) and val != id else val
-    
+    # Caso seja um ponteiro (string) ou o próprio literal
+    if isinstance(res, str) and res in GLOBALS or (current_frame() and res in current_frame().variables):
+        return to_value(res)
+        
     return id
 
 def set_value(id, val):
     global GLOBALS, STACK
     
-    def cast_value(old_val, new_val):
-        if old_val is None: return new_val
+    # Helper para definir o nome do tipo
+    def get_type_name(v):
+        if isinstance(v, float): return 'float'
+        if isinstance(v, int): return 'int'
+        if isinstance(v, list): return 'array'
+        return 'str'
+
+    # Helper para realizar o casting e manter o dicionário
+    def prepare_storage(old_data, new_val):
+        if old_data is None or not isinstance(old_data, dict):
+            # Criação inicial da variável
+            return {'value': new_val, 'type': get_type_name(new_val)}
+        
+        # Atualização: Mantém o tipo original e faz o cast do novo valor
+        t = old_data['type']
         try:
-            if isinstance(old_val, float): return float(new_val)
+            if t == 'float': final_val = float(new_val)
+            elif t == 'int': final_val = int(new_val)
+            else: final_val = new_val
+        except:
+            final_val = new_val
             
-            if isinstance(old_val, int): return int(new_val)
-            
-            return type(old_val)(new_val)
-        except (ValueError, TypeError):
-            return new_val
+        return {'value': final_val, 'type': t}
 
     if '$' not in id:
         if id in GLOBALS:
-            GLOBALS[id] = cast_value(GLOBALS[id], val)
+            GLOBALS[id] = prepare_storage(GLOBALS[id], val)
         else:
             frame = current_frame()
             if frame:
                 if id not in frame.variables:
                     frame.new_var(id)
-
-                old_val = frame.get_var(id)
-                GLOBALS[id] = cast_value(old_val, val) if old_val is not None else val
-                frame.set_var(id, GLOBALS[id])
+                old = frame.get_var(id)
+                data = prepare_storage(old, val)
+                frame.set_var(id, data)
             else:
-                GLOBALS[id] = val
+                GLOBALS[id] = prepare_storage(None, val)
     else:
+        # Para Arrays
         name, pos = id.split('$')
-        pos = int(to_value(pos))
+        pos_idx = int(to_value(pos))
         
         target_array = GLOBALS.get(name)
         if target_array is None and current_frame():
             target_array = current_frame().get_var(name)
+
+        target_array = target_array['value']
         
         if target_array is not None:
-            target_array[pos] = cast_value(target_array[0], val)
+            # Assume-se que o array guarda dicionários em cada posição
+            target_array[pos_idx] = prepare_storage(target_array[pos_idx], val)
 
 def LOAD():
     global PC
@@ -228,26 +248,42 @@ def BLE():
 def PARAM():
     global PC, PARAMETERS
     a = get_addresses()[0]
-    PARAMETERS.append(to_value(a))
+
+    val = to_value(a)
+    
+    type = 'float' if isinstance(val, float) else 'int'
+    if isinstance(val, str): type = 'str'
+    
+    PARAMETERS.append({'value': val, 'type': type})
     
     PC += 1
 
 def CALL():
     global PC, PARAMETERS, STACK, LABELS
     a, b = get_addresses()
+    
+    num_params = int(to_value(b))
+    
     params = {}
-    for i in range(to_value(b)):
-        aux = PARAMETERS.pop(0)
-        params[f'a{i}'] = to_value(aux)
+    for i in range(num_params):
+        if PARAMETERS:
+            params[f'a{i}'] = PARAMETERS.pop(0)
+    
     PARAMETERS.clear()
     
-    STACK.append(Frame(PC+1,params))
-    PC = LABELS.get(a,None)
+    STACK.append(Frame(PC+1, params))
+    PC = LABELS.get(a, None)
 
 def RETURN():
     global PC, STACK, GLOBALS
     a = get_addresses()[0]
-    GLOBALS['ra'] = to_value(a)
+    
+    val = to_value(a)
+    type = 'float' if isinstance(val, float) else 'int'
+    if isinstance(val, str): type = 'str'
+    
+    GLOBALS['ra'] = {'value': val, 'type': type}
+    
     PC = current_frame().static_link
     STACK.pop()
 
@@ -263,7 +299,7 @@ def READLN():
     addresses = get_addresses()
     a = addresses[0]
     b = input()
-    # Tenta converter input para número se aplicável
+    
     val = is_number(b) if is_number(b) is not None else b
     set_value(a, val)
     PC += 1
@@ -272,13 +308,13 @@ def ALLOC():
     global PC, GLOBALS, STACK
     a, b, c = get_addresses()
 
-    b = int(to_value(b))
-    c = to_value(c)
-
-    arr = [c]*b
+    size = int(to_value(b))
+    initial_val = to_value(c)
+    type_name = 'float' if isinstance(initial_val, float) else 'int'
+    
+    arr = [{'value': initial_val, 'type': type_name} for _ in range(size)]
 
     set_value(a, arr)
-    
     PC += 1
 
 def main():
